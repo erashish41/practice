235f9971a2a7b0e87e33e4db9636141960f0a4eb
ashish <erashish41@gmail.com>
2024-05-18 15:23:27 +0530

    map and filter added

---
+++ JS/01_conversions/07_mapreducefilter.js
@@ -0,0 +1,28 @@
+// Map, Filter, Reduce
+
+// 1. Map -- The map() method is used for creating a new array 
+//           from an existing one, applying a function to each one of the elements of the first array.
+
+
+const count = [1, 2, 3, 4, 5, 6, 7]
+
+// count = current element, i = index, arr = acutal array
+const threeMultipleCount = count.map( (count, i, arr) => {       
+     count = count * 3 + i
+    return count
+})
+console.log(threeMultipleCount);
+
+
+
+// 2. Filter -- The filter() method takes each element in an array and it applies a conditional statement
+//              against it. If this conditional returns true, the element gets pushed to the output array.
+//              If the condition returns false, the element does not get pushed to the output array.
+
+
+const count1 = [1, 2, 3, 4, 5]
+
+const greterThanTwo = count1.filter( (count, i, arr) => {
+    return count > 2
+})
+console.log(greterThanTwo);
