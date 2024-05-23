a51a082aa8f5b3bdf0188beeb1ff51045bed1561
ashish <erashish41@gmail.com>
2024-05-23 19:15:03 +0530

    polyfill for reduce

--- JS/01_conversions/07_mapreducefilter.js
+++ JS/01_conversions/07_mapreducefilter.js
@@ -1,5 +1,6 @@
 // // Map, Filter, Reduce
 // //  Polyfill for Map, Filter, Re0duce
+// // Practice on Arrays also in last
 
 // // 1. Map -- The map() method is used for creating a new array 
 // //           from an existing one, applying a function to each one of the elements of the first array.
@@ -45,21 +46,61 @@
 
 // console.log(sum);
 
+
+
+
 // *************************************||************************************************
 
 //  Prototype allows you to add new properties and methods to arrays.
-// 1. Polyfill for map()
 
-Array.prototype.count = function (cb) {
-    let temp = [];
-    for (let i = 0; i < array.length; i++) {
-        temp.push(cb(this[i], i, this));
+// // 1. Polyfill for map()
+// Array.prototype.myMap = function (cb) {
+//     let temp = [];
+//     for (let i = 0; i < array.length; i++) {
+//         temp.push(cb(this[i], i, this));
         
-    }
-}
+//     }
+//     return temp;
+// }
 
-const count = [1, 2, 3, 4, 5]
-const twoMultiCount = count.map( (count,i, arr) => {
-    return count = count * 2 + i
-})
-console.log(twoMultiCount);
+// const count = [1, 2, 3, 4, 5]
+// const twoMultiCount = count.map( (count,i, arr) => {
+//     return count = count * 2 + i
+// })
+// console.log(twoMultiCount);
+
+
+// // 2. Polyfill for filter()
+// array.prototype.myFilter = function (cb){
+//     let temp = []
+//     for (let i = 0; i < array.length; i++) {
+//         if (cb(this[i], i, this))temp.push(this[i])
+//     }
+//     return temp;
+// }
+// const count1 = [1, 2, 3, 4, 5]
+
+// const greterThanTwo = count1.myFilter( (count1, i, arr) => {
+//     count1 =  count1 > 2
+//     return count1
+// })
+// console.log(greterThanTwo);
+
+// // 3. Polyfill for reduce()
+// Array.prototype.myReduce = function (cb, initialValue){
+//     var accumulator = initialValue;
+
+//     for(let i=0; i<this.length; i++ ){
+//         accumulator = accumulator ? cb(accumulator, this[i], i, this) : this[i];
+//     }
+//     return accumulator;
+// }
+// const count2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// const sum = count2.myReduce( (acc, curr, i, arr) => {
+//     return acc + curr;
+// },0)
+
+// console.log(sum);
+
+// Practice on Arrays 
