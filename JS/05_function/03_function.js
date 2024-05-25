e2dc1d378b7e7599f6f4e1dfda7d207712327e0d
ashish <erashish41@gmail.com>
2024-05-26 00:15:24 +0530

    function u

---
+++ JS/05_function/03_function.js
@@ -0,0 +1,9 @@
+// 1. what is Function declaration
+function sum(num) {
+    return num * num
+}
+
+// 2.  what is Function Expersion
+const square = function(num){           // this is called Anonymous function -- It is a function that does not have any name associated with it.
+    return num * num
+}
