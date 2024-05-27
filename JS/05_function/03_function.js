00976afbb6d0bd72dab5e1a5f74594260456f71b
ashish <erashish41@gmail.com>
2024-05-28 00:01:44 +0530

    update

--- JS/05_function/03_function.js
+++ JS/05_function/03_function.js
@@ -3,7 +3,7 @@ function sum(num) {
     return num * num
 }
 
-// 2.  what is Function Expersion
+// 2.  what is Function Expersion.
 const square = function(num){           // this is called Anonymous function -- It is a function that does not have any name associated with it.
     return num * num
 }
