f24bce34ad1850929e43338390b6d46071c441c4
ashish <erashish41@gmail.com>
2024-05-29 00:06:51 +0530

    first class function

--- JS/05_function/03_function.js
+++ JS/05_function/03_function.js
@@ -1,15 +1,20 @@
 // // 1. what is Function declaration
 // function sum(num) {
 //     return num * num
-// }
+// }        
 
-// 2.  what is Function Expersion.
-// const square = function(num){           // this is called Anonymous function -- The function which does not have any name associated with it.
+// // 2.  what is Function Expersion.
+// const square1 = function (num) {           // this is called Anonymous function -- The function which that does not have any name associated with it.
 //     return num * num
 // }
-// console.log(square(5));
+// console.log(square1(8));
 
-const square = function (num) {
-    return num * num 
+// 3. what are First class Function ?   --if functions in that language are treated like other variables. So the functions can be assigned to any other variable or passed as an argument or can be returned by another function. 
+function square2(mul){
+    return mul * mul
 }
-square(2)
+
+function doubleSq (nu) {
+    console.log( "square is" + nu(4));
+}
+console.log(square2);
