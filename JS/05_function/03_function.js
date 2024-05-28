a70578a239140ac8e945a6c353ca5f6fd4845306
ashish <erashish41@gmail.com>
2024-05-28 23:44:35 +0530

    function up

--- JS/05_function/03_function.js
+++ JS/05_function/03_function.js
@@ -1,9 +1,15 @@
-// 1. what is Function declaration
-function sum(num) {
-    return num * num
-}
+// // 1. what is Function declaration
+// function sum(num) {
+//     return num * num
+// }
 
 // 2.  what is Function Expersion.
-const square = function(num){           // this is called Anonymous function -- It is a function that does not have any name associated with it.
-    return num * num
+// const square = function(num){           // this is called Anonymous function -- The function which does not have any name associated with it.
+//     return num * num
+// }
+// console.log(square(5));
+
+const square = function (num) {
+    return num * num 
 }
+square(2)
