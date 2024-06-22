0c7bca6f8825bfc8c365435ef40ec51bee2d3b19
ashish <erashish41@gmail.com>
2024-06-23 00:26:04 +0530

    object example 2

--- JS/08_object/03_object.js
+++ JS/08_object/03_object.js
@@ -21,12 +21,28 @@
 // }
 
 
-// 1. what is output of this data
-const data = {
-    a : "one",
-    b : "two",
-    a : "three",
-};
-console.log( data);  // keys with same name will replace the first key with second key
+// // 1. what is output of this data
+// const data = {
+//     a : "one",
+//     b : "two",
+//     a : "three",
+// };
+// console.log( data);  // keys with same name will replace the first key with second key and last key will become first key in output
 
 // 2. 
+let nums = {
+    a: 100,
+    b: 200,
+    title: "My nums",
+};
+
+multiplyByTwo(obj);
+
+function multiplyByTwo(obj) {
+    for (key in obj) {
+     if (typeof obj[key] ===     "number") {
+        obj[key] *= 2;
+        }
+    }
+}
+console.log(nums);
