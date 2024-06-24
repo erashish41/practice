856568229c813d6e6b5b6633eb2188290d00a285
ashish <erashish41@gmail.com>
2024-06-25 00:00:09 +0530

    object ex 3

--- JS/08_object/03_object.js
+++ JS/08_object/03_object.js
@@ -29,19 +29,30 @@
 // };
 // console.log( data);  // keys with same name will replace the first key with second key and last key will become first key in output
 
-// 2. 
-let nums = {
-    a: 100,
-    b: 200,
-    title: "My nums",
-};
-multiplyByTwo(nums);
+// // 2. what is output of this data
+// let nums = {
+//     a: 100,
+//     b: 200,
+//     title: "My nums",
+// };
+// multiplyByTwo(nums);
 
-function multiplyByTwo(obj) {           // loop object can be done by (key in obj)
-    for (key in obj) {
-     if (typeof obj[key] === "number") {
-        obj[key] *= 2;
-        }
-    }
-}
-console.log(nums);
+// function multiplyByTwo(obj) {           // loop object can be done by (key in obj)
+//     for (key in obj) {
+//      if (typeof obj[key] === "number") {
+//         obj[key] *= 2;
+//         }
+//     }
+// }
+// console.log(nums);
+
+
+// 3. what is output of this data
+const a = {};
+const b = { key: "b"};
+const c = { key: "c"};
+
+a[b] = 123;
+a[c] = 456;
+
+console.log(a[b]);
